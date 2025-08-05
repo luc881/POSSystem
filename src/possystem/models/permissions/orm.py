7297279fde5508a68e15af2f19ee65ab510d2563
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-08-05 03:52:53 -0600

    Bidirectional relationship with secondary created at roles and permissions

--- src/possystem/models/permissions/orm.py
+++ src/possystem/models/permissions/orm.py
@@ -1,7 +1,7 @@
 from sqlalchemy import String, BigInteger, TIMESTAMP
 from sqlalchemy.sql import func
 from ...db.session import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Permission(Base):
     __tablename__ = "permissions"
@@ -10,3 +10,9 @@ class Permission(Base):
     name: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     created_at: Mapped = mapped_column(TIMESTAMP(timezone=False), server_default=func.now())
     updated_at: Mapped = mapped_column(TIMESTAMP(timezone=False), onupdate=func.now())
+
+    roles: Mapped[list["Role"]] = relationship(
+        "Role",
+        secondary="role_has_permissions",  # pivot table name
+        back_populates="permissions"
+    )
--- src/possystem/models/roles/orm.py
+++ src/possystem/models/roles/orm.py
@@ -13,3 +13,9 @@ class Role(Base):
     updated_at: Mapped = mapped_column(TIMESTAMP(timezone=False), onupdate=func.now())
 
     users: Mapped[list["User"]] = relationship("User", back_populates="role")
+
+    permissions: Mapped[list["Permission"]] = relationship(
+        "Permission",
+        secondary="role_has_permissions",  # pivot table name
+        back_populates="roles"
+    )
