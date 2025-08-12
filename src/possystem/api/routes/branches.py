2fbc8ff8cd6a6bec8f683ef964c3b194cd4d816d
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-08-12 00:25:34 -0600

    Branches post endpoint added

--- src/possystem/api/routes/branches.py
+++ src/possystem/api/routes/branches.py
@@ -3,7 +3,7 @@ from starlette import status
 from ...models.branches.orm import Branch  # Import Branch ORM
 from typing import Annotated
 from sqlalchemy.orm import Session
-from ...models.branches.schemas import BranchResponse # Import UserResponse schema
+from ...models.branches.schemas import BranchResponse,BranchBase # Import UserResponse schema
 from ...db.session import get_db  # Use the shared one
 
 db_dependency = Annotated[Session, Depends(get_db)]
@@ -23,3 +23,21 @@ router = APIRouter(
 async def read_all(db: db_dependency):
     branches = db.query(Branch).all()
     return branches
+
+@router.post('/',
+            status_code=status.HTTP_201_CREATED,
+            response_model=BranchResponse,
+            summary="Create a new branch",
+            description="Adds a new branch to the database. The branch name must be unique.")
+async def create_branch(db: db_dependency, branch_request: BranchBase):
+    branch_model = Branch(**branch_request.model_dump())
+
+    branch_found = db.query(Branch).filter(Branch.name.ilike(branch_model.name)).first()
+
+    if branch_found:
+        raise HTTPException(status_code=409, detail='Branch already exists')
+
+    db.add(branch_model)
+    db.commit()
+    db.refresh(branch_model)
+    return branch_model
