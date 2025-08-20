bbb82a18e721fbb548e5e85ee34cdd0f7026bddb
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-08-20 16:32:40 -0600

    product post endpoint added

--- src/possystem/api/routes/products.py
+++ src/possystem/api/routes/products.py
@@ -26,3 +26,21 @@ async def read_all(db: db_dependency):
     products = db.query(Product).all()
     return products
 
+@router.post("/",
+            response_model=ProductResponse,
+            summary="Create a new product",
+            description="Create a new product with the provided details.",
+            status_code=status.HTTP_201_CREATED,
+            dependencies=CAN_CREATE_PRODUCTS)
+async def create_product(product: ProductCreate, db: db_dependency):
+    existing_product = db.query(Product).filter(Product.sku == product.sku).first()
+    if existing_product:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Product with this SKU already exists."
+        )
+    new_product = Product(**product.model_dump())
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
