36a2f11bbff133683b1e7d46365e821656daccab
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-09-02 00:24:17 -0600

    Purchase detail post endpoint added

--- src/possystem/api/routes/purchase_details.py
+++ src/possystem/api/routes/purchase_details.py
@@ -32,3 +32,34 @@ router = APIRouter(
 async def read_all(db: db_dependency):
     purchase_details = db.query(PurchaseDetail).all()
     return purchase_details
+
+
+@router.post(
+    "/",
+    response_model=PurchaseDetailResponse,
+    summary="Create a new purchase detail",
+    description="Create a new purchase detail with the provided information.",
+    status_code=status.HTTP_201_CREATED,
+    dependencies=CAN_CREATE_PURCHASE_DETAILS
+)
+async def create(purchase_detail: PurchaseDetailCreate, db: db_dependency):
+    # Check if the associated purchase exists
+    purchase = db.query(Purchase).filter(Purchase.id == purchase_detail.purchase_id).first()
+    if not purchase:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Associated purchase not found")
+
+    # Check if the associated product exists
+    product = db.query(Product).filter(Product.id == purchase_detail.product_id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Associated product not found")
+
+    # Check if the associated unit exists
+    unit = db.query(Unit).filter(Unit.id == purchase_detail.unit_id).first()
+    if not unit:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Associated unit not found")
+
+    new_purchase_detail = PurchaseDetail(**purchase_detail.model_dump())
+    db.add(new_purchase_detail)
+    db.commit()
+    db.refresh(new_purchase_detail)
+    return new_purchase_detail
