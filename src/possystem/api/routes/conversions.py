eacac11931b64626870611100a544b72170337df
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-09-03 14:11:54 -0600

    Conversions post endpoint added

--- src/possystem/api/routes/conversions.py
+++ src/possystem/api/routes/conversions.py
@@ -33,4 +33,41 @@ async def read_all(db: db_dependency):
     conversions = db.query(Conversion).all()
     return conversions
 
-#
+@router.post(
+    "/",
+    response_model=ConversionResponse,
+    summary="Create a new conversion",
+    description="Create a new conversion with the provided details.",
+    status_code=status.HTTP_201_CREATED,
+    dependencies=CAN_CREATE_CONVERSIONS
+)
+async def create(conversion: ConversionCreate, db: db_dependency):
+    # Check if the associated warehouses exist
+    warehouse = db.query(Warehouse).filter(Warehouse.id == conversion.warehouse_id).first()
+    if not warehouse:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Source warehouse not found")
+
+    # Check if the associated product exists
+    product = db.query(Product).filter(Product.id == conversion.product_id).first()
+    if not product:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Associated product not found")
+
+    # Check if the associated unit exists
+    unit_start = db.query(Unit).filter(Unit.id == conversion.unit_start_id).first()
+    if not unit_start:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Starting unit not found")
+
+    unit_end = db.query(Unit).filter(Unit.id == conversion.unit_end_id).first()
+    if not unit_end:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Ending unit not found")
+
+    # Check if the associated user exists
+    user = db.query(User).filter(User.id == conversion.user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Associated user not found")
+
+    new_conversion = Conversion(**conversion.model_dump())
+    db.add(new_conversion)
+    db.commit()
+    db.refresh(new_conversion)
+    return new_conversion
