eca02cbac04aff62b1a33c7e5774bb36f9b2b4f3
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-10-28 18:15:57 -0600

    product batch post validation for invalid product id added

--- src/possystem/api/routes/product_batch.py
+++ src/possystem/api/routes/product_batch.py
@@ -7,6 +7,8 @@ from ...models.permissions.orm import Permission
 from ...utils.permissions import CAN_READ_PRODUCT_BATCHES, CAN_CREATE_PRODUCT_BATCHES, CAN_UPDATE_PRODUCT_BATCHES, CAN_DELETE_PRODUCT_BATCHES
 from ...models.product_batch.orm import ProductBatch
 from ...models.product_batch.schmas import ProductBatchCreate, ProductBatchResponse, ProductBatchUpdate, ProductBatchDetailsResponse
+from ...models.products.orm import Product
+
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
@@ -31,6 +33,12 @@ def read_all_product_batches(db: db_dependency):
              status_code=status.HTTP_201_CREATED,
              dependencies=CAN_CREATE_PRODUCT_BATCHES)
 def create_product_batch(product_batch: ProductBatchCreate, db: db_dependency):
+    existing_product = db.query(Product).filter(Product.id == product_batch.product_id).first()
+    if not existing_product:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Product with the given ID does not exist."
+        )
     new_product_batch = ProductBatch(**product_batch.model_dump())
     db.add(new_product_batch)
     db.commit()
