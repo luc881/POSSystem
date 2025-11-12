44cc6f3b4c8d5b839d592ee61f20235f3f85396b
Luciano <ljuarezs2000@alumno.ipn.mx>
2025-11-11 23:23:57 -0600

    permissions file updated for deleted tables

--- src/possystem/utils/permissions.py
+++ src/possystem/utils/permissions.py
@@ -26,18 +26,6 @@ CAN_CREATE_ROLES = [Depends(require_permission("roles.create"))]
 CAN_UPDATE_ROLES = [Depends(require_permission("roles.update"))]
 CAN_DELETE_ROLES = [Depends(require_permission("roles.delete"))]
 
-# Permissions for unit operations
-CAN_READ_UNITS = [Depends(require_permission("units.read"))]
-CAN_CREATE_UNITS = [Depends(require_permission("units.create"))]
-CAN_UPDATE_UNITS = [Depends(require_permission("units.update"))]
-CAN_DELETE_UNITS = [Depends(require_permission("units.delete"))]
-
-# Permissions for warehouse operations
-CAN_READ_WAREHOUSES = [Depends(require_permission("warehouses.read"))]
-CAN_CREATE_WAREHOUSES = [Depends(require_permission("warehouses.create"))]
-CAN_UPDATE_WAREHOUSES = [Depends(require_permission("warehouses.update"))]
-CAN_DELETE_WAREHOUSES = [Depends(require_permission("warehouses.delete"))]
-
 # Permissions for product operations
 CAN_READ_PRODUCTS = [Depends(require_permission("products.read"))]
 CAN_CREATE_PRODUCTS = [Depends(require_permission("products.create"))]
@@ -49,30 +37,6 @@ CAN_READ_PRODUCT_CATEGORIES = [Depends(require_permission("productscategories.re
 CAN_CREATE_PRODUCT_CATEGORIES = [Depends(require_permission("productscategories.create"))]
 CAN_UPDATE_PRODUCT_CATEGORIES = [Depends(require_permission("productscategories.update"))]
 CAN_DELETE_PRODUCT_CATEGORIES = [Depends(require_permission("productscategories.delete"))]
-
-# Permissions for product warehouse operations
-CAN_READ_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.read"))]
-CAN_CREATE_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.create"))]
-CAN_UPDATE_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.update"))]
-CAN_DELETE_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.delete"))]
-
-# Permissions for product wallet operations
-CAN_READ_PRODUCT_WALLETS = [Depends(require_permission("productwallets.read"))]
-CAN_CREATE_PRODUCT_WALLETS = [Depends(require_permission("productwallets.create"))]
-CAN_UPDATE_PRODUCT_WALLETS = [Depends(require_permission("productwallets.update"))]
-CAN_DELETE_PRODUCT_WALLETS = [Depends(require_permission("productwallets.delete"))]
-
-# Permissions for product stock initial operations
-CAN_READ_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.read"))]
-CAN_CREATE_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.create"))]
-CAN_UPDATE_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.update"))]
-CAN_DELETE_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.delete"))]
-
-# Permissions for client operations
-CAN_READ_CLIENTS = [Depends(require_permission("clients.read"))]
-CAN_CREATE_CLIENTS = [Depends(require_permission("clients.create"))]
-CAN_UPDATE_CLIENTS = [Depends(require_permission("clients.update"))]
-CAN_DELETE_CLIENTS = [Depends(require_permission("clients.delete"))]
 
 # Permissions for sales operations
 CAN_READ_SALES = [Depends(require_permission("sales.read"))]
@@ -91,12 +55,6 @@ CAN_READ_SALE_DETAILS = [Depends(require_permission("saledetails.read"))]
 CAN_CREATE_SALE_DETAILS = [Depends(require_permission("saledetails.create"))]
 CAN_UPDATE_SALE_DETAILS = [Depends(require_permission("saledetails.update"))]
 CAN_DELETE_SALE_DETAILS = [Depends(require_permission("saledetails.delete"))]
-
-# Permissions for sale detail attention operations
-CAN_READ_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.read"))]
-CAN_CREATE_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.create"))]
-CAN_UPDATE_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.update"))]
-CAN_DELETE_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.delete"))]
 
 # Permissions for refund product operations
 CAN_READ_REFUND_PRODUCTS = [Depends(require_permission("refundproducts.read"))]
@@ -122,22 +80,66 @@ CAN_CREATE_PURCHASE_DETAILS = [Depends(require_permission("purchasedetails.creat
 CAN_UPDATE_PURCHASE_DETAILS = [Depends(require_permission("purchasedetails.update"))]
 CAN_DELETE_PURCHASE_DETAILS = [Depends(require_permission("purchasedetails.delete"))]
 
-# Permissions for transport operations
-CAN_READ_TRANSPORTS = [Depends(require_permission("transports.read"))]
-CAN_CREATE_TRANSPORTS = [Depends(require_permission("transports.create"))]
-CAN_UPDATE_TRANSPORTS = [Depends(require_permission("transports.update"))]
-CAN_DELETE_TRANSPORTS = [Depends(require_permission("transports.delete"))]
-
-# Permissions for conversion operations
-CAN_READ_CONVERSIONS = [Depends(require_permission("conversions.read"))]
-CAN_CREATE_CONVERSIONS = [Depends(require_permission("conversions.create"))]
-CAN_UPDATE_CONVERSIONS = [Depends(require_permission("conversions.update"))]
-CAN_DELETE_CONVERSIONS = [Depends(require_permission("conversions.delete"))]
-
 # Permissions for product batch operations
 CAN_READ_PRODUCT_BATCHES = [Depends(require_permission("productbatches.read"))]
 CAN_CREATE_PRODUCT_BATCHES = [Depends(require_permission("productbatches.create"))]
 CAN_UPDATE_PRODUCT_BATCHES = [Depends(require_permission("productbatches.update"))]
 CAN_DELETE_PRODUCT_BATCHES = [Depends(require_permission("productbatches.delete"))]
+
+# Permissions for unit operations
+# CAN_READ_UNITS = [Depends(require_permission("units.read"))]
+# CAN_CREATE_UNITS = [Depends(require_permission("units.create"))]
+# CAN_UPDATE_UNITS = [Depends(require_permission("units.update"))]
+# CAN_DELETE_UNITS = [Depends(require_permission("units.delete"))]
+
+# Permissions for warehouse operations
+# CAN_READ_WAREHOUSES = [Depends(require_permission("warehouses.read"))]
+# CAN_CREATE_WAREHOUSES = [Depends(require_permission("warehouses.create"))]
+# CAN_UPDATE_WAREHOUSES = [Depends(require_permission("warehouses.update"))]
+# CAN_DELETE_WAREHOUSES = [Depends(require_permission("warehouses.delete"))]
+
+# Permissions for product warehouse operations
+# CAN_READ_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.read"))]
+# CAN_CREATE_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.create"))]
+# CAN_UPDATE_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.update"))]
+# CAN_DELETE_PRODUCT_WAREHOUSES = [Depends(require_permission("productwarehouses.delete"))]
+
+# Permissions for product wallet operations
+# CAN_READ_PRODUCT_WALLETS = [Depends(require_permission("productwallets.read"))]
+# CAN_CREATE_PRODUCT_WALLETS = [Depends(require_permission("productwallets.create"))]
+# CAN_UPDATE_PRODUCT_WALLETS = [Depends(require_permission("productwallets.update"))]
+# CAN_DELETE_PRODUCT_WALLETS = [Depends(require_permission("productwallets.delete"))]
+
+# Permissions for product stock initial operations
+# CAN_READ_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.read"))]
+# CAN_CREATE_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.create"))]
+# CAN_UPDATE_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.update"))]
+# CAN_DELETE_PRODUCT_STOCK_INITIALS = [Depends(require_permission("productstockinitials.delete"))]
+
+# Permissions for client operations
+# CAN_READ_CLIENTS = [Depends(require_permission("clients.read"))]
+# CAN_CREATE_CLIENTS = [Depends(require_permission("clients.create"))]
+# CAN_UPDATE_CLIENTS = [Depends(require_permission("clients.update"))]
+# CAN_DELETE_CLIENTS = [Depends(require_permission("clients.delete"))]
+
+# Permissions for sale detail attention operations
+# CAN_READ_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.read"))]
+# CAN_CREATE_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.create"))]
+# CAN_UPDATE_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.update"))]
+# CAN_DELETE_SALE_DETAIL_ATTENTIONS = [Depends(require_permission("saledetailattentions.delete"))]
+
+# Permissions for transport operations
+# CAN_READ_TRANSPORTS = [Depends(require_permission("transports.read"))]
+# CAN_CREATE_TRANSPORTS = [Depends(require_permission("transports.create"))]
+# CAN_UPDATE_TRANSPORTS = [Depends(require_permission("transports.update"))]
+# CAN_DELETE_TRANSPORTS = [Depends(require_permission("transports.delete"))]
+
+# Permissions for conversion operations
+# CAN_READ_CONVERSIONS = [Depends(require_permission("conversions.read"))]
+# CAN_CREATE_CONVERSIONS = [Depends(require_permission("conversions.create"))]
+# CAN_UPDATE_CONVERSIONS = [Depends(require_permission("conversions.update"))]
+# CAN_DELETE_CONVERSIONS = [Depends(require_permission("conversions.delete"))]
+
+
 
 
